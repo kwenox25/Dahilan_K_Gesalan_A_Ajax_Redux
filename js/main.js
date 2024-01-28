603116dfd38858aded7d33491c61062df616833e
kwenox25 <113370944+kwenox25@users.noreply.github.com>
2024-01-28 01:40:54 -0500

    added greensocks

--- js/main.js
+++ js/main.js
@@ -4,59 +4,64 @@
   const filmTemplate = document.querySelector("#movie-template");
   const filmCon = document.querySelector("#film-con");
   const filmBox = document.querySelector("#film-box");
-  const baseUrl = `https://swapi.dev/api/`;
+
+  // Set initial styles for the film box
+  gsap.set("#film-box", { autoAlpha: 0, x: -200 });
 
   function getCharacters() {
-    fetch(`${baseUrl}people/?page=2`)
+    fetch(`https://swapi.dev/api/people/?page=2`)
       .then((response) => response.json())
       .then(function (response) {
         console.log(response.results);
         const character = response.results;
         const ul = document.createElement("ul");
+
         character.forEach((movie) => {
           const li = document.createElement("li");
           const a = document.createElement("a");
-          //  console.log(movie.name);
-          //console.log(movie.films[0]);
-          a.classList.add("character-link");
 
+          a.classList.add("character-link");
           a.textContent = movie["name"];
           a.dataset.film = movie.films[0];
 
           li.appendChild(a);
           ul.appendChild(li);
         });
+
         characterBox.appendChild(ul);
       })
       .then(function () {
         const links = document.querySelectorAll("#character-box li a");
         links.forEach((link) => {
-          link.addEventListener("click", getFilm);
+          link.addEventListener("click", function (e) {
+            getFilm(e);
+          });
         });
       })
       .catch((error) => {
-        console.error(
-          "You don't have internet or your internet is slow, try again later.",
-          error
-        );
-        mainDiv.classList.add("error-style");
-        mainDiv.classList.remove("grid-con");
-        mainDiv.innerHTML =
-          "Error.<br>Some data requirements failed to load.<br>Please try again later.";
+        console.error("Error fetching characters:", error);
+        handleFetchError();
       });
   }
+
   function getFilm(e) {
     const filmID = e.currentTarget.dataset.film;
-    //https://swapi.dev/api/films/1/
 
+    gsap.to("#film-box", {
+      duration: 0.5,
+      autoAlpha: 0,
+      x: -200,
+      onComplete: function () {
+        fetchFilmDetails(filmID);
+      },
+    });
+  }
+
+  function fetchFilmDetails(filmID) {
     fetch(`${filmID}`)
       .then((response) => response.json())
       .then(function (response) {
-        filmCon.innerHTML = "";
-        //  filmBox.innerHTML = "";
-
         const template = document.importNode(filmTemplate.content, true);
-
         const reviewBody = template.querySelector(".review-description");
         const movieTitle = template.querySelector(".movie-title");
         const img = document.createElement("img");
@@ -66,18 +71,26 @@
         img.src = `images/${response.episode_id}.jpg`;
 
         template.appendChild(img);
+        filmCon.innerHTML = "";
         filmCon.appendChild(template);
+
+        gsap.to("#film-box", {
+          duration: 0.5,
+          autoAlpha: 1,
+          x: 0,
+        });
       })
       .catch((error) => {
-        console.error(
-          "You don't have internet or your internet is slow, try again later.",
-          error
-        );
-        mainDiv.classList.add("error-style");
-        mainDiv.classList.remove("grid-con");
-        mainDiv.innerHTML =
-          "Error.<br>Some data requirements failed to load.<br>Please try again later.";
+        console.error("Error fetching film details:", error);
+        handleFetchError();
       });
+  }
+
+  function handleFetchError() {
+    mainDiv.classList.add("error-style");
+    mainDiv.classList.remove("grid-con");
+    mainDiv.innerHTML =
+      "Error.<br>Some data requirements failed to load.<br>Please try again later.";
   }
 
   getCharacters();
